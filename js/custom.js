8125a2ecf650d3ee95739df0d3ff3f5b800681d7
anisurrahmanshohag <76775918+anisurrahmanshohag@users.noreply.github.com>
2021-06-29 18:21:08 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+$(function() {
+    'use strict';
+
+
+
+
+    // =============model click=========
+    $('.read_button').on('click', function() {
+        $('.perag').addClass('overflow_visible')
+
+    });
+
+    $('.read_button').on('click', function() {
+        $('.read_button').addClass('overflow_hidden')
+
+    });
+    $('.read_button').on('click', function() {
+        $('.signature').addClass('signature_down')
+
+    });
+
+
+    // =============model click=========
+
+
+
+
+
+
+});
